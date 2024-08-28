116e6f15a00945c898fd224d9c2cc3cdb0fd453d
Robert Mordzon <robertmordzon@gmail.com>
2024-08-28 23:42:40 +0200

    New style CSS

--- main.py
+++ main.py
@@ -45,33 +45,175 @@ def status_page():
 
     # Define the HTML template with auto-refresh every 2 seconds
     html_template = '''
-    <html>
-    <head>
-        <title>Web3Pi Status Page</title>
-        <meta http-equiv="refresh" content="5">
-    </head>
-    <body>
-        <h1>Web3Pi status: {{ status }}</h1>
-        <br><hr><br>
-        <b>hostname:</b> <a href="http://{{ hostname }}.local">{{ hostname }}</a><br>
-        <b>IP:</b> <a href="http://{{ IP }}">{{ IP }}</a><br>
-        <b>uptime:</b> {{ uptime }}<br><br>
-        <b>Grafana:</b> <a href="http://{{ IP }}:3000/dashboards">http://{{ IP }}:3000/dashboards</a><br>
-        <b>Simple System Monitor:</b> <a href="http://{{ IP }}:7197/node/system/status">http://{{ IP }}:7197/node/system/status</a><br>
-        <hr>
-        Page generation time: {{ gTime }} <br>
-        <hr>
+    <!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="refresh" content="5">
+    <title>Web3Pi Installation Status</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            background-color: #121212;
+            color: #e0e0e0;
+            margin: 0;
+            padding: 20px;
+            display: flex;
+            flex-direction: column;
+            height: 100vh;
+            box-sizing: border-box;
+        }
+
+        img.logo {
+            width: 150px;
+            margin-bottom: 20px;
+        }
+
+        h1, h2, h3, h4 {
+            margin: 10px 0;
+        }
+
+        h1 {
+            font-size: 1.5em;
+            color: #00ffcc;
+        }
+
+        .status {
+            margin-top: 20px;
+            margin-bottom: 20px;
+            line-height: 1.6;
+        }
+
+        .links {
+            margin-top: 30px;
+            padding-top: 20px;
+            border-top: 1px solid #333;
+            display: flex;
+            justify-content: space-around;
+        }
+
+        .links a {
+            color: #00aced;
+            text-decoration: none;
+            border: 2px solid #00aced;
+            padding: 10px 20px;
+            border-radius: 5px;
+            transition: background-color 0.3s, color 0.3s;
+        }
+
+        .links a:hover {
+            background-color: #00aced;
+            color: #121212;
+        }
+
+        .link-url {
+            margin-top: 10px;
+            text-align: center;
+            font-size: 0.9em;
+            color: #888;
+            word-wrap: break-word;
+        }
+
+        .link-container {
+            text-align: center;
+            margin-top: 10px;
+        }
+
+        .log-container {
+            flex-grow: 1;
+            margin-top: 20px;
+            background-color: #1e1e1e;
+            padding: 15px;
+            border-radius: 5px;
+            overflow: hidden;
+            display: flex;
+            flex-direction: column;
+        }
+
+        textarea {
+            flex-grow: 1;
+            width: 100%;
+            background-color: #1e1e1e;
+            color: #e0e0e0;
+            border: none;
+            resize: none;
+            outline: none;
+            font-family: monospace;
+            font-size: 0.9em;
+            line-height: 1.4;
+            padding: 0;
+            margin: 0;
+            overflow-y: scroll;
+        }
+
+        textarea::-webkit-scrollbar {
+            width: 8px;
+        }
+
+        textarea::-webkit-scrollbar-thumb {
+            background-color: #00aced;
+            border-radius: 4px;
+        }
+
+        textarea::-webkit-scrollbar-track {
+            background-color: #333;
+        }
+        
+        .top-right {
+            position: absolute;
+            top: 20px;
+            right: 20px;
+        }
+
+        .top-right a {
+            color: #00aced;
+            text-decoration: none;
+            font-size: 1em;
+            transition: color 0.3s;
+        }
+
+        .top-right a:hover {
+            color: #00ffcc;
+        }
+    </style>
+</head>
+<body>
+
+    <div class="top-right">
+        <a href="https://setup-guide.web3pi.io/GetStart/single-device/" target="_blank">Documentation</a>
+    </div>
+
+     <a href="https://web3pi.io" target="_blank">
+        <img src="https://cdn.prod.website-files.com/66531c1b16371fa0309a0de0/66a3c94b4154857fba1bf1ff_Web3Pi%20Logo%20new2.svg" alt="Web3Pi Logo" class="logo">
+    </a>
+    
+    <div class="status">
+        <h1>Web3Pi installation status: {{ status }}</h1>
+        <h2>Node IP: <a href="http://{{ IP }}" class="link-url" target="_blank">{{ IP }}</a></h2>
+        <h3>Hostname: <a href="http://{{ hostname }}.local" class="link-url" target="_blank">{{ hostname }}.local</a></h3>
+        <h4>Uptime: {{ uptime }}</h4>
+        <h4>Page generation time: {{ gTime }}</h4>
+    </div>
+
+    <div class="links">
+        <a href="http://{{ IP }}:3000" target="_blank"><center>Grafana monitoring<br>http://{{ IP }}:3000</center></a>
+        <a href="http://{{ IP }}:7197/node/system/status" target="_blank"><center>JSON status<br>http://{{ IP }}:7197/node/system/status</center></a>
+    </div>
+
+    <div class="log-container">
         Installation log file (/var/log/web3pi.log)<br><br>
-        <textarea id="logTextArea" rows="50" cols="200">{{ log }}</textarea><br>
-        <hr>
-        <script type="text/javascript">
-            window.onload = function () {
-            var textarea = document.getElementById('logTextArea');
-            textarea.scrollTop = textarea.scrollHeight;
-            }
-        </script>
-    </body>
-    </html>
+        <textarea readonly id="log-content">{{ log }}</textarea>
+    </div>
+
+    <script>
+        // Auto-scroll to the bottom of the textarea
+        const textarea = document.getElementById('log-content');
+        textarea.scrollTop = textarea.scrollHeight;
+    </script>
+</body>
+</html>
+
     '''
 
     # Render the HTML template with the status value
